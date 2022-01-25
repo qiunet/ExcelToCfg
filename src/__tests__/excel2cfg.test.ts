2c38389716407c4c006166e23851a039361f1f8b
qiunet <qiunet@163.com>
2022-01-25 13:56:48 +0800

    1.0.19

--- src/__tests__/excel2cfg.test.ts
+++
@@ -1,6 +0,0 @@
-import { ExcelToCfg, Role } from '../index';
-
-test("convert", () => {
-    let excelToCfg = new ExcelToCfg(Role.SERVER, "G全局表_global_setting.xlsx", __dirname, __dirname);
-    excelToCfg.convert()
-});
